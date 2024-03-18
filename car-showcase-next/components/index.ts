2471f0a381f29099ce58544b845861d3912152c0
gpapathimiu <gracepapathimiu@yahoo.com>
2024-03-18 16:02:52 +0100

    car showcase with next and apis

---
+++ car-showcase-next/components/index.ts
@@ -0,0 +1,24 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import SearchBar from "./SearchBar";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+import SearchManufacturer from "./SearchManufacturer";
+import CustomFilter from "./CustomFilter";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+
+export {
+    Hero,
+    CustomButton,
+    SearchBar,
+    CarCard,
+    CarDetails,
+    SearchManufacturer,
+    CustomFilter,
+    ShowMore,
+    Navbar,
+    Footer,
+}

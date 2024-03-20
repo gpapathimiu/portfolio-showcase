2485cb7666c251dc2f1c15a3a0beec07db8b359c
Grejs Papathimiu <147829023+gpapathimiu@users.noreply.github.com>
2024-03-20 13:42:08 +0100

    Update Navbar.jsx

--- arthub-full/client/src/components/Navbar.jsx
+++ arthub-full/client/src/components/Navbar.jsx
@@ -4,74 +4,72 @@ import { useState, useEffect } from "react";
 import axios from 'axios';
 
 const Navbar = () => {
-	const { logout } = useAuth();
-	const navigate = useNavigate();
+    const { logout } = useAuth();
+    const navigate = useNavigate();
 
-	const userId = localStorage.getItem("userId");
+    const userId = localStorage.getItem("userId");
 
-	const [loggedUser, setLoggedUser] = useState({});
+    const [loggedUser, setLoggedUser] = useState({});
 
-	useEffect(() => {
-		axios
-			.get(`http://localhost:8000/api/user/${userId}`, {
-				withCredentials: true,
-			})
-			.then((res) => {
-				setLoggedUser(res.data.user);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, []);
+    useEffect(() => {
+        axios
+            .get(`http://localhost:8000/api/user/${userId}`, {
+                withCredentials: true,
+            })
+            .then((res) => {
+                setLoggedUser(res.data.user);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
-	const handleLogout = async (e) => {
-		e.preventDefault();
+    const handleLogout = async (e) => {
+        e.preventDefault();
 
-		try {
-			await logout();
-			navigate("/");
-		} catch (err) {
-			console.log(err);
-		}
-	};
+        try {
+            await logout();
+            navigate("/");
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-	return (
-		<nav className="border-gray-200 bg-blue-800">
-			<div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-2">
-				<a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-					<img src="/logo.png" className="h-16" alt="Arthub Logo" />
-					<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-						Arthub
-					</span>
-				</a>
-				<div className="flex items-center space-x-6 rtl:space-x-reverse">
-					{userId ? (
-						<>
-							<a
-								href="/post/add"
-								className="text-lg  text-gray-500 dark:text-white hover:underline">
-								Upload
-							</a>
-							<a
-								href={`/user/${userId}`}
-								className="text-lg  text-gray-500 dark:text-white hover:underline">
-								Your Profile
-							</a>
-							<button
-						onClick={handleLogout}
-						className="bg-indigo-500 hover:bg-orange-400 border-none rounded-full text-white font-semibold  w-20 h-20 flex items-center justify-center">
-						<Link to="/" className="hover:no-underline">
-							<p className="text-white">Logout</p>
-						</Link>
-					</button>
-				</div>
-						</>
-					) : (
-						<></>
-					)}
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="border-gray-200 bg-blue-800">
+            <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-2">
+                <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="/logo.png" className="h-16" alt="Arthub Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                        Arthub
+                    </span>
+                </Link>
+                <div className="flex items-center space-x-6 rtl:space-x-reverse">
+                    {userId ? (
+                        <>
+                            <Link
+                                to="/post/add"
+                                className="text-lg  text-gray-500 dark:text-white hover:underline">
+                                Upload
+                            </Link>
+                            <Link
+                                to={`/user/${userId}`}
+                                className="text-lg  text-gray-500 dark:text-white hover:underline">
+                                Your Profile
+                            </Link>
+                            <button
+                                onClick={handleLogout}
+                                className="bg-indigo-500 hover:bg-orange-400 border-none rounded-full text-white font-semibold  w-20 h-20 flex items-center justify-center">
+                                Logout
+                            </button>
+                        </>
+                    ) : (
+                        <></>
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
